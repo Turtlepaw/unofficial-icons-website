0f785654ef830719b2992135e2c1f149d906ae83
TurtlePaw <81275769+TurtlePaw@users.noreply.github.com>
2022-03-21 23:31:39 -0400

    Fix

---
+++ Pepperbot/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using IconsDiscord;
+using Radzen;
+using Blazored.Modal;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+//var builder = WebAssemblyHostBuilder.CreateDefault(args);
+//builder.RootComponents.Add<App>("app");
+
+
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
+builder.Services.AddBlazoredModal();
+
+//builder.Services.AddOidcAuthentication(options =>
+//{
+//    // Configure your authentication provider options here.
+//    // For more information, see https://aka.ms/blazor-standalone-auth
+//    builder.Configuration.Bind("Local", options.ProviderOptions);
+//});
+
+var app = builder.Build();
+
+await app.RunAsync();
